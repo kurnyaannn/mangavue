d37e1896fdf8194be26230d65f3071398717504b
kwrnya <y21kurnia@gmail.com>
2021-05-27 21:05:25 +0700

    changing LoadingDetails component layout

--- src/components/LoadingDetails.vue
+++ src/components/LoadingDetails.vue
@@ -1,16 +1,16 @@
 <template>
 	<div class="flex flex-col">
-		<div class="border border-gray-400 rounded-md w-full py-3 pl-3">
-			  <div class="animate-pulse flex space-x-4">
+		<div class="border border-gray-400 rounded-md w-full py-3 px-3 md:pl-3">
+			  <div class="animate-pulse flex flex-col md:flex-row md:space-x-4">
 			  	<div class="flex flex-col">
 			      <div class="h-4 mb-2 w-full bg-gray-400 rounded"></div>
-			    	<div class="bg-gray-400 w-48 h-36 mb-2"></div>
+			    	<div class="bg-gray-400 w-full md:w-48 h-36 mb-2"></div>
 			    	<div class="h-4 bg-gray-400 rounded w-4/6 mb-1"></div>
 			    	<div class="h-4 bg-gray-400 rounded w-3/6 mb-2"></div>
 			    	<div class="h-4 bg-gray-400 rounded w-4/6 mb-1"></div>
 			    	<div class="h-4 bg-gray-400 rounded w-3/6 mb-2"></div>
 			  	</div>
-			    <div class="flex-1 space-y-1 py-3 pr-3">
+			    <div class="flex-row md:flex-1 md:space-y-1 py-3 md:pr-3">
 			      <div class="space-y-2 mb-4">
 			        <div class="h-4 bg-gray-400 rounded"></div>
 			        <div class="h-4 bg-gray-400 rounded"></div>
